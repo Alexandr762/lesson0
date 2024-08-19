2d781d5708f190b0ede5a76c4c5f565339bec321
Alexandr762 <aleksandrromaskin373@gmail.com>
2024-08-19 17:55:38 +0400

    Add files via upload

---
+++ varriable.py
@@ -0,0 +1,5 @@
+number_of_homework = 12
+time = 1.5
+course_name = "Python"
+one_task = time / number_of_homework
+print("Курс:",course_name,"," "всего задач:",number_of_homework,",","затрачено часов:",time,",","среднее время выполнения:",one_task,"часа.")

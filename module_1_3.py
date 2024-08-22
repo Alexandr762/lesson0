eb969fb13d84e25c8090ac54ac84eea2c86e7ed8
Alexandr762 <aleksandrromaskin373@gmail.com>
2024-08-22 19:22:41 +0400

    Add files via upload

--- module_1_3.py
+++ module_1_3.py
@@ -2,7 +2,7 @@ name = "Alexandr"
 print(name ,type(name))
 age = 30
 print(age ,type(age))
-new_age = age + 1
-print(new_age ,type(new_age))
+age = age + 1
+print(age ,type(age))
 is_student = True
-print(True ,type(True))
+print(is_student ,type(is_student))

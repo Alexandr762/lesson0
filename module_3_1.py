012feaf5be52220efd4e10503da8fc6fb9479bf3
Alexandr762 <aleksandrromaskin373@gmail.com>
2024-09-12 23:20:42 +0400

    Add files via upload

--- module_3_1.py
+++ module_3_1.py
@@ -8,16 +8,18 @@ def string_info(string):
     length = len(string)
     uppercase = string.upper()
     lowercase = string.lower()
+    count_calls()
     return length, uppercase, lowercase
 
 def is_contains(string, list_to_search):
     for item in list_to_search:
         if string.lower() == item.lower():
+            count_calls()
             return True
     return False
 
 
-for _ in range(11):
+for _ in range(2):
     count_calls()
     string_info("Привет")
     is_contains("привет", ["hello", "world"])
